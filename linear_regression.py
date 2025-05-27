7ac045f54d2ba5d026f85c6c0df2935723641791
monikadhok <monikadhok014@gmail.com>
2025-05-27 11:08:46 +0530

    Create linear_regression.py

---
+++ linear_regression.py
@@ -0,0 +1,68 @@
+#pip install keras~=3.8.0 matplotlib~=3.10.0 numpy~=2.0.0 pandas~=2.2.0 tensorflow~=2.18.0
+#print('\n\nAll requirements successfully installed.')
+#Load dependencies
+#general
+import io
+# data
+import numpy as np
+import pandas as pd
+# machine learning
+import keras
+# data visualization
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import seaborn as sns
+
+#Read the dataset 
+chicago_taxi_dataset = pd.read_csv("https://download.mlcc.google.com/mledu-datasets/chicago_taxi_train.csv")
+
+# Updates dataframe to use specific columns.
+training_df = chicago_taxi_dataset[['TRIP_MILES', 'TRIP_SECONDS', 'FARE', 'COMPANY', 'PAYMENT_TYPE', 'TIP_RATE']]
+
+print('Read dataset completed successfully.')
+print('Total number of rows: {0}\n\n'.format(len(training_df.index)))
+training_df.head(200)
+
+## View dataset characteristics 
+print('Total number of rows: {0}\n\n'.format(len(training_df.index)))
+training_df.describe(include='all')
+
+
+
+answer = '''
+What is the maximum fare? 				              Answer: $159.25
+What is the mean distance across all trips? 		Answer: 8.2895 miles
+How many cab companies are in the dataset? 		  Answer: 31
+What is the most frequent payment type? 		    Answer: Credit Card
+Are any features missing data? 				          Answer: No
+'''
+
+# You should be able to find the answers to the questions about the dataset
+# by inspecting the table output after running the DataFrame describe method.
+#
+# Run this code cell to verify your answers.
+
+# What is the maximum fare?
+max_fare = training_df['FARE'].max()
+print("What is the maximum fare? \t\t\t\tAnswer: ${fare:.2f}".format(fare = max_fare))
+
+# What is the mean distance across all trips?
+mean_distance = training_df['TRIP_MILES'].mean()
+print("What is the mean distance across all trips? \t\tAnswer: {mean:.4f} miles".format(mean = mean_distance))
+
+# How many cab companies are in the dataset?
+num_unique_companies =  training_df['COMPANY'].nunique()
+print("How many cab companies are in the dataset? \t\tAnswer: {number}".format(number = num_unique_companies))
+
+# What is the most frequent payment type?
+most_freq_payment_type = training_df['PAYMENT_TYPE'].value_counts().idxmax()
+print("What is the most frequent payment type? \t\tAnswer: {type}".format(type = most_freq_payment_type))
+
+# Are any features missing data?
+missing_values = training_df.isnull().sum().sum()
+print("Are any features missing data? \t\t\t\tAnswer:", "No" if missing_values == 0 else "Yes")
+
+
+
+
